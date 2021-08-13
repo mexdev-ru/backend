34e2ae8dfc1201abacafd21b43b83e1aea29143a
Daniil Shilov <shilov.da2001@mail.ru>
2021-08-13 15:56:54 +0300

    Initialize database

--- src/main/resources/database/initDB.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS codes
-(
-    id  uuid ,
-    code  VARCHAR(20) NOT NULL ,
-    typeOfVocabulary  VARCHAR(10) NOT NULL ,
-    coderOrganization  VARCHAR(3) NOT NULL ,
-    version  VARCHAR(35) NOT NULL
-);
