bf71918c4e18b922ecba95622671449b0de4748b
Daniil Shilov <shilov.da2001@mail.ru>
2021-08-17 13:05:36 +0300

    Add error handler
    Error message: field name and message

---
+++ src/main/java/ru/mexdev/application/error/ErrorHandlingController.java
@@ -0,0 +1,35 @@
+package ru.mexdev.application.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+import javax.validation.ConstraintViolationException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class ErrorHandlingController {
+
+  @ResponseBody
+  @ExceptionHandler(ConstraintViolationException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public List<Violation> onConstraintValidationException(ConstraintViolationException e) {
+    return e.getConstraintViolations().stream()
+        .map(violation -> new Violation(violation.getPropertyPath().toString(), violation.getMessage()))
+        .collect(Collectors.toList());
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ResponseBody
+  public List<Violation> onMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    return e.getBindingResult().getFieldErrors().stream()
+        .map(error -> new Violation(error.getField(), error.getDefaultMessage()))
+        .collect(Collectors.toList());
+  }
+}
